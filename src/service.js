740f356a76a8b9db5c2f64cf991ded73a419c568
“r0556727863” <r0556727863@gmail.com>
2025-02-05 17:47:59 +0200

    “change-client”

--- src/service.js
+++ src/service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = 'http://localhost:5053'; // שנה לכתובת ה-API שלך
+const apiUrl = process.env.REACT_APP_API_URL; 
 axios.defaults.baseURL = apiUrl;
 
 axios.interceptors.response.use(
